a83e9548bf3e3f084b7d25f612acdaca547f4e35
Matteo Carpi <matteocarpi94@gmail.com>
2020-03-25 13:03:35 +0100

    feat: styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import styles from '../styles/Home.module.scss';
+
 const Home = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Tameto Gatsby</h1>
+    <div className={styles.content}>
+      <h1>Tameto Gatsby</h1>
+    </div>
   </Layout>
 );
 
